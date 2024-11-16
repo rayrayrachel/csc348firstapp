9abe280163dab09c1d2c1a05b7e40be3dedbfd59
rayrayrachel <rachellamwingtung@gmail.com>
2024-11-16 09:34:32 +0000

    [AUTHENTICATION] setup breeze authentication

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "This is the /home page";
+    return view('welcome');
 });
 
-Route::get('/blog', function () {
-    return "This is the /blog page";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
