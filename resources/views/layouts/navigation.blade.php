95a9a6e9f309b61414112e47b872eb1d6a67b5b9
rayrayrachel <rachellamwingtung@gmail.com>
2024-11-22 14:12:29 +0000

    [LAYOUT] sticky navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,9 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+
+<nav x-data="{ open: false }" class = "navbar-sticky-top">
+    <div class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
+
+    <div class="below-sticky">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
@@ -14,7 +18,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('bloggers')" :active="request()->routeIs('bloggers')">
                         {{ __('Bloggers') }}
@@ -108,4 +112,7 @@
             </div>
         </div>
     </div>
+    </div>
+</div>
 </nav>
+
