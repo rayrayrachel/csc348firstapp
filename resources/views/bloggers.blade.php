56ff4773fe9fa6834550167d68bd06ea4f56e5f0
rayrayrachel <rachellamwingtung@gmail.com>
2024-11-23 14:56:10 +0000

    [UI] testing with css

--- resources/views/bloggers.blade.php
+++ resources/views/bloggers.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Bloggers') }}
+        <h2 class="pageHeader">
+            {{ __('All Bloggers') }}
         </h2>
     </x-slot>
 
